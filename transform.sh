21e5abd60f0d4347d93246e3ffb285b7be333b79
fernandosanchezmunoz <fernandosanchezmunoz@gmail.com>
2017-04-04 12:20:31 -0400

    modify examples to data

--- transform.sh
+++ transform.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
+APP_NAME="$1"
+BASE_DIR="./data/"
+WORKING_DIR=$BASE_DIR$APP_NAME
+CURRENT_DIR=$PWD
+
 #read example name from first argument
 if [ -z "$1" ]; then
   echo "** ERROR: no parameter received. Enter the name of the subdirectory on ./examples to convert"
-  ls -A1l ./examples | grep ^d | awk '{print $9}'
+  ls -A1l $BASE_DIR | grep ^d | awk '{print $9}'
   exit 1
 fi
 
 #change to subdirectory
-cd ./examples/"$1"
+cd $WORKING_DIR
 
-container-transform -i compose -o marathon docker-compose.yml > marathon.json && \
-../../container_transform/marathon_group.py -i marathon.json -n ${PWD##*/}  > group.json && \
+container-transform -i compose -o marathon docker-compose.yml > marathon.json 
+echo "***** MARATHON.JSON *****"
+cat marathon.json
+../../container_transform/marathon_group.py -i marathon.json -n ${PWD##*/}  > group.json
+echo "***** GROUP.JSON *****"
+cat group.json
 dcos marathon group add ./group.json
 
-cd ../..
+cd $CURRENT_DIR
 
 exit 0
