1d9d37022a371129c1472ae8382e6caa6706ab03
fernandosanchezmunoz <fernandosanchezmunoz@gmail.com>
2017-04-04 12:23:42 -0400

    add documentation

--- transform.sh
+++ transform.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#   Reads a "docker-compose.yml" file located in a directory with the application name:
+# (e.g. ./data/redis/docker-compose.yml).
+#  Creates a marathon.json out of it with a list of the containers included in the YML. 
+#  Finally processes that list and builds a Marathon group out of it, modifying some parameters
+# to adapt it to a DC/OS cluster (e.g. using Marathon's dynamic port assignment and Marathon-LB). 
+
 APP_NAME="$1"
 BASE_DIR="./data/"
 WORKING_DIR=$BASE_DIR$APP_NAME
