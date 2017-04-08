9d3a139aad96a8be0a8126cf1d42f7cc46fd389a
fernandosanchezmunoz <fernandosanchezmunoz@gmail.com>
2017-04-08 13:54:35 -0400

    mkdir data_dir

--- container_transform/marathon_group.py
+++ container_transform/marathon_group.py
@@ -158,6 +158,13 @@ def	copy_content_to_external_volume( external_volume_name, source_path, mount_pa
 	proc = subprocess.Popen( command, stdout=subprocess.PIPE, shell=True)
 	(out, err) = proc.communicate()
 
+	data_dir="data"
+	print("**DEBUG: mkdir {0} ".format(source_path, mount_point+"/"+mount_path+"/"+source_path[2:]+"/"data_dir))
+	#mkdir "/data"
+	command = "mkdir "+mount_point+"/"+mount_path+"/"+source_path[2:]+"/"data_dir
+	proc = subprocess.Popen( command, stdout=subprocess.PIPE, shell=True)
+	(out, err) = proc.communicate()
+
 	#copy source to mount_path - not needed, just need to CREATE mount_path
 	#command = "cp -R "+source_path+" "+mount_point+"/"+mount_path
 	#proc = subprocess.Popen( command, stdout=subprocess.PIPE, shell=True)
