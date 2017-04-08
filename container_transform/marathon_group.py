bec4d54d990a229dd0161a4bb122cb0c22da0c23
fernandosanchezmunoz <fernandosanchezmunoz@gmail.com>
2017-04-08 13:44:22 -0400

    copy recursively to dest path too

--- container_transform/marathon_group.py
+++ container_transform/marathon_group.py
@@ -143,10 +143,18 @@ def	copy_content_to_external_volume( external_volume_name, source_path, mount_pa
 	(out, err) = proc.communicate()	
 
 	#recursively copy the content
-	print("**DEBUG: COPY from {0} to {1}".format(source_path, mount_point+"/"+dest_path))
+	print("**DEBUG: COPY from {0} to {1}".format(source_path, mount_point+"/"+src_path))
 
 	#copy source to src_path
 	command = "cp -R "+source_path+" "+mount_point+"/"+source_path
+	proc = subprocess.Popen( command, stdout=subprocess.PIPE, shell=True)
+	(out, err) = proc.communicate()
+
+	#recursively copy the content
+	print("**DEBUG: COPY from {0} to {1}".format(source_path, mount_point+"/"+mount_path+"/"+source_path)
+
+	#copy source to mount_path/src_path
+	command = "cp -R "+source_path+" "+mount_point+"/"+mount_path+"/"+source_path
 	proc = subprocess.Popen( command, stdout=subprocess.PIPE, shell=True)
 	(out, err) = proc.communicate()
 
