636da8e9c8778a4ccf318759e999407aac97979a
fernandosanchezmunoz <fernandosanchezmunoz@gmail.com>
2017-04-09 18:07:19 -0400

    user absolute path for staging dir

--- container_transform/marathon_group.py
+++ container_transform/marathon_group.py
@@ -249,11 +249,11 @@ def create_artifact_from_volume( volume, app_name, app_server_address ):
 	#create subdir for staging with containerpath
 	#staging_dir = staging_mount_point+"/"+container_path[1:]+"/"
 	if os.path.isdir(container_path):
-		container_basename = container_path
+		container_dir = container_path
 		print("**DEBUG: is dir: {0}".format(os.path.basename(container_basename)) ) #remove leading slash
 	else:
-		container_basename = os.path.basename(container_path)
-		print("**DEBUG: is file: {0} and basename is {1}".format(container_path, container_basename) ) #remove leading slash		
+		container_dir = os.path.dirname(container_path)
+		print("**DEBUG: is file: {0} and dirname is {1}".format(container_path, container_dir) ) #remove leading slash		
 
 	staging_dir = staging_mount_point+"/"+app_name+"/"+container_basename #/tmp/ctransform/nginx-php/etc/nginx/conf.d
 	print("**DEBUG: Create staging dir {0}".format(staging_dir) ) #remove leading slash
