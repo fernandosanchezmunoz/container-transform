e593079962bc67a5b38a62f6838bda0454f8a53d
fernandosanchezmunoz <fernandosanchezmunoz@gmail.com>
2017-04-09 18:17:35 -0400

    user absolute path for staging dir

--- container_transform/marathon_group.py
+++ container_transform/marathon_group.py
@@ -270,7 +270,7 @@ def create_artifact_from_volume( volume, app_name, app_server_address ):
 
 	#compress staging_dir to artifact
 	print("**DEBUG: Compress {0} into {1} with relative path {2}".format(staging_app_dir, artifact_name,staging_app_dir ))
-	command = "tar -C "+staging_mount_point+" -cvzf "+artifact_name+" "+host_path #compress this directory
+	command = "tar -v -C "+staging_mount_point+" -cvzf "+artifact_name+" "+host_path #compress this directory
 	proc = subprocess.Popen( command, stdout=subprocess.PIPE, shell=True)
 	(out, err) = proc.communicate()
 
