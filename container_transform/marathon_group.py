496b6cf91001ae4ca9418c7e923f789adcb069af
fernandosanchezmunoz <fernandosanchezmunoz@gmail.com>
2017-04-09 16:01:38 -0400

    compress to staging dir with containerpath

--- container_transform/marathon_group.py
+++ container_transform/marathon_group.py
@@ -283,8 +283,8 @@ def modify_volume_for_uri( volume, app_name, app_server_address ):
 	(out, err) = proc.communicate()		
 
 	#copy contents to staging dir
-	print("**DEBUG: Copy {0} into {1}".format(host_path, staging_dir))
-	command = "cp -r "+host_path+" "+staging_dir
+	print("**DEBUG: Copy {0} into {1}".format(host_path"/*", staging_dir))
+	command = "cp -r "+host_path+"/* "+staging_dir
 	proc = subprocess.Popen( command, stdout=subprocess.PIPE, shell=True)
 	(out, err) = proc.communicate()		
 
