449666f2b31ccef4f7859200ff079f4838f39ca9
fernandosanchezmunoz <fernandosanchezmunoz@gmail.com>
2017-04-10 22:59:26 -0400

    add pods

--- container_transform/marathon_group.py
+++ container_transform/marathon_group.py
@@ -64,8 +64,8 @@ def adapt_containers_to_pod( containers, name ):
 			endpoint['protocol'] = [ portMapping['protocol'] ]
 			temp_container['endpoints'] += endpoint
 		temp_container['image'] = { } 
-		temp_container['image']['kind']: container['type']
-		temp_container['image']['id']: container['docker']['image']
+		temp_container['image']['kind'] = container['type']
+		temp_container['image']['id'] = container['docker']['image']
 		pod_containers += container
 	print("**DEBUG: pod_containers is {0}".format(containers))
 
