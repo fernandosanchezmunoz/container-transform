a5b0a14d4b427e726cc66a397e81253da9c039a5
fernandosanchezmunoz <fernandosanchezmunoz@gmail.com>
2017-04-13 22:49:54 -0400

    uris to artifacts

--- container_transform/marathon_group.py
+++ container_transform/marathon_group.py
@@ -81,7 +81,7 @@ def adapt_apps_to_pod( apps, name, app_server_address ):
 		temp_app['image'] = { } 
 		temp_app['image']['kind'] = container['type']
 		temp_app['image']['id'] = container['docker']['image']
-		temp_app['artifacts'] = app_uris.get( 'uris', [''] )
+		temp_app['artifacts'] = app_uris.get( 'uris', ['http://172.31.46.13/web-fernandosanchez_02-express-redis-nodemon-_src_app_app.tgz'] )
 
 		print("**DEBUG: temp_app is {0}".format(temp_app))
 		pod_apps.append(temp_app)
