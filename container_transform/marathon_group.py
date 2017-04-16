cfe5c6f5a50173acc5f0509ac47a365db6159538
fernandosanchezmunoz <fernandosanchezmunoz@gmail.com>
2017-04-16 16:17:51 -0400

    WIP

--- container_transform/marathon_group.py
+++ container_transform/marathon_group.py
@@ -94,7 +94,7 @@ def adapt_apps_to_pod( apps, name, app_server_address ):
 			endpoint['hostPort'] = portMapping['hostPort']
 			endpoint['protocol'] = [ portMapping['protocol'] ]
 			#endpoint['labels'] = { "VIP_0": "/"+name+":"+str(portMapping['hostPort']) }
-			endpoint['labels'] = { "VIP_0": "/composeapp:"+endpoint['hostPort'] }
+			endpoint['labels'] = { "VIP_0": "/composeapp:"+str(endpoint['hostPort']) }
 			#kk
 			temp_app['endpoints'].append(endpoint)
 		temp_app['image'] = { } 
